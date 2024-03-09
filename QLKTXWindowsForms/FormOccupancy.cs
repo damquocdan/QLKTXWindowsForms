abae032649ab3126f33dfcbb68bbec4a79aee5e8
Admin <Admin@DESKTOP-UT0KFJN>
2024-03-09 18:24:08 +0700

    new update

--- QLKTXWindowsForms/FormOccupancy.cs
+++ QLKTXWindowsForms/FormOccupancy.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Principal;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace QLKTXWindowsForms
 {
     public partial class FormOccupancy : Form
     {
         QLKTXEntities db = new QLKTXEntities();
-
+        private int checkStatus = -1;
         public FormOccupancy()
         {
             InitializeComponent();
@@ -22,18 +19,77 @@ namespace QLKTXWindowsForms
         public void LoadHopDong()
         {
             var listHopDong = from c in db.Occupancies
-                              select new { c.OccupancyID, c.Student.FullName, c.Room.NumberRoom,c.RenewalDate, c.ExpirationDate,c.CycleMonths,c.Status };
+                              select new { c.OccupancyID, c.Student.StudentCode, c.Room.NumberRoom,c.RenewalDate, c.ExpirationDate,c.CycleMonths,c.Status };
             dgvHopDong.DataSource = listHopDong.ToList();
         }
         private void button5_Click(object sender, EventArgs e)
         {
-
+            txtID.Text = "";
+            textBoxMaSinhVien.Text = "";
+            textBoxMaPhong.Text = "";
+            textBoxHanPhong.Text = "";
+            LoadHopDong();
+            checkBoxStatus.Checked = false;
+            checkStatus = -1;
         }
+        private void checkBoxStatus_CheckedChanged(object sender, EventArgs e)
+        {
+            checkStatus = 0;
 
+            if (checkBoxStatus.Checked)
+            {
+                checkStatus = 1;
+            }
+            
+        }
         private void btnTraCuu_Click(object sender, EventArgs e)
         {
+            int id = SystemQLKT.StringToInt(txtID.Text);
+            string maSv = textBoxMaSinhVien.Text;
+            int maPhong = SystemQLKT.StringToInt(textBoxMaPhong.Text);
+            int hanphong = SystemQLKT.StringToInt(textBoxHanPhong.Text);
 
+            var listHD = from c in db.Occupancies select c;
+
+            if (id != 0)
+            {
+                listHD = listHD.Where(c => c.OccupancyID == id);
+            }
+            if (!maSv.Equals(""))
+            {
+                listHD = listHD.Where(c => c.Student.StudentCode.Contains(maSv));
+            }
+            if (maPhong != 0)
+            {
+                listHD = listHD.Where(c => c.RoomID == maPhong);
+            }
+
+            if (hanphong != 0)
+            {
+                DateTime ngaySoSanh = DateTime.Today.AddDays(hanphong * 30);
+                listHD = listHD.Where(c => c.ExpirationDate >= DateTime.Today && c.ExpirationDate < ngaySoSanh);
+            }
+            if (checkStatus == 1 || checkStatus == 0)
+            {
+                bool result = (checkStatus != 0);
+                listHD = listHD.Where(c => c.Status == result);
+            }
+
+            var hd = from c in listHD
+                     select new
+                     {
+                         c.OccupancyID,
+                         c.Student.StudentCode,
+                         c.Room.NumberRoom,
+                         c.RenewalDate,
+                         c.ExpirationDate,
+                         c.CycleMonths,
+                         c.Status
+                     };
+
+            dgvHopDong.DataSource = hd.ToList();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -43,6 +99,35 @@ namespace QLKTXWindowsForms
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = label1.Text;
+            ExportExcel.DataToExcel(dgvHopDong, text);
+        }
+
+        private void buttonNotifixcation_Click(object sender, EventArgs e)
+        {
+            string email = "21111064572@hunre.edu.vn";
+            string passwork = "Danli29.03";
+            string subject = "Ký túc xá trường đại học tài nguyên và môi trường";
+            foreach (DataGridViewRow row in dgvHopDong.Rows)
+            {
+                string studentCode = row.Cells[1].Value.ToString();
+                var listName = (from c in db.Students
+                              where c.StudentCode.Equals(studentCode)
+                              select c).FirstOrDefault();
+                string name = !string.IsNullOrEmpty(listName.FullName) ? listName.FullName : studentCode;
+                string htmlBody = "<div>" +
+                                    "<h2>Dear "+ name + "!</h2>" +
+                                    "<p>Mời bạn vào trang web ký túc xá để gia hạn phòng!</p>" +
+                                    "<a href=\"#\">Nhấp vào đây để tới trang web</a>" +
+                               "</div>";
+                string toEmail = studentCode + "@hunre.edu.vn";
+                SystemQLKT.SendEmail(email, passwork, toEmail, subject, htmlBody);
+            }
+            MessageBox.Show("Gửi email thông báo thành công!", "Thông báo");
         }
     }
 }
---
+++ QLKTXWindowsForms/System.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLKTXWindowsForms
+{
+    internal class SystemQLKT
+    {
+        public static int StringToInt(string str)
+        {
+            if (int.TryParse(str, out int output))
+            {
+                return output;
+            }
+
+            return 0;
+        }
+        public static void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra nếu ký tự nhập vào không phải là số và không phải là phím điều hướng (ví dụ: phím Back)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                // Chặn ký tự nhập vào bằng cách xử lý sự kiện KeyPress
+                e.Handled = true;
+            }
+        }
+        public static void SendEmail(string fromEmail, string password, string toEmail, string subject, string htmlBody)
+        {
+            try
+            {
+                // Khởi tạo đối tượng SmtpClient
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(fromEmail, password);
+
+                // Tạo đối tượng MailMessage
+                MailMessage mailMessage = new MailMessage(fromEmail, toEmail, subject, htmlBody);
+                mailMessage.IsBodyHtml = true;
+
+                // Gửi email
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending email: " + ex.Message);
+            }
+        }
+    }
+}
